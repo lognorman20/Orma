b4202d3faa8ddcc736e4cb68bf035145d2c10086
Logan Norman <lognorman20@gmail.com>
2025-08-09 15:53:16 -0700

    Initial Commit

---
+++ Orma/OrmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrmaApp.swift
+//  Orma
+//
+//  Created by Logan Norman on 8/9/25.
+//
+
+import SwiftUI
+
+@main
+struct OrmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OrmaTests/OrmaTests.swift
@@ -0,0 +1,17 @@
+//
+//  OrmaTests.swift
+//  OrmaTests
+//
+//  Created by Logan Norman on 8/9/25.
+//
+
+import Testing
+@testable import Orma
+
+struct OrmaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
